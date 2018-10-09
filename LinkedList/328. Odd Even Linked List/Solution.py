c8ddb734e7b72317173f8c7db2b1563801a6702c
JingRachaelZhu <43859168+JingRachaelZhu@users.noreply.github.com>
2018-10-09 12:51:39 -0400

    Create Solution.py

---
+++ LinkedList/328. Odd Even Linked List/Solution.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        odd =head
+        evenhead =head.next
+        even =head.next
+        while even and even.next:
+            odd.next =even.next
+            odd =odd.next
+            even.next =even.next.next
+            even =even.next
+        odd.next =evenhead
+        
+        return head
+            
+        
+        
