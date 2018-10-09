69df1447b2c30065ee305a2da786b3f9a817e614
JingRachaelZhu <43859168+JingRachaelZhu@users.noreply.github.com>
2018-10-09 11:50:07 -0400

    Create Solution2.py

---
+++ LinkedList/24. Swap Nodes in Pairs/Solution2.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        second =head.next
+        head.next =self.swapPairs(second.next)
+        second.next =head
+        
+        return second
