c15b08699b7f1102c06c47010df6d097edca9935
Jing Zhu <jzhu76@binghamton.edu>
2018-10-28 17:21:38 -0400

    update

--- Array/80. Remove Duplicates from Sorted Array II/Solution2.py
+++ Array/80. Remove Duplicates from Sorted Array II/Solution2.py
@@ -6,7 +6,7 @@ class Solution:
         """
         ct =0
         for num in nums:
-        	if i<2 or num >nums[i-2]:		#it at most appears twice 
+        	if i<2 or num >nums[ct-2]:		#it at most appears twice in the updated array 
         		nums[ct] =num
         		ct +=1
         return ct
