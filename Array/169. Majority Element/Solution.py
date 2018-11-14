f137b52eba05c88fbb580b8924f345165a5bcf25
Jing Zhu <jzhu76@binghamton.edu>
2018-11-14 15:05:21 -0500

    Update Array/169. Majority Element/Solution.py

--- Array/169. Majority Element/Solution.py
+++ Array/169. Majority Element/Solution.py
@@ -10,9 +10,8 @@ class Solution:
         return res[len(nums)//2]
 
         #hashtable(dict)
-<<<<<<< HEAD
-  #       dic1 =[]
-  #       res =0
+        # dic1 =[]
+        # res =0
 		# for i in nums:
 		# 	# if i not in dic1:
 		# 	# 	dic1[i] =1
@@ -22,16 +21,5 @@ class Solution:
 		# 	if dic1.get(i,0)>len(nums)//2:
 		# 		res =i
 		# return res
-=======
-        dic1 =[]
-        res =0
-	for i in nums:
-		# if i not in dic1:
-		# 	dic1[i] =1
-		# else:
-		# 	dic1[i] +=1
-		dic1[i] +=dic1.get(i,0)
-		if dic1.get(i,0)>len(nums)//2:
-			res =i
-	return res
->>>>>>> 096e33cee4a942c0907a93ec08435b359e026ad0
+
+
