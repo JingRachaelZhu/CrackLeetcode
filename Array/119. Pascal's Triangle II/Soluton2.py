1627d56a87ae0f0de570ef038f4d833542d1c691
JingRachaelZhu <43859168+JingRachaelZhu@users.noreply.github.com>
2018-11-14 14:22:17 -0500

    Update Soluton2.py

--- Array/119. Pascal's Triangle II/Soluton2.py
+++ Array/119. Pascal's Triangle II/Soluton2.py
@@ -11,8 +11,8 @@ class Solution:
         return res
 
         #use map,(Return an iterator on Python3 while a list on Python2) 
-		res =[[1]]
-		for i in range(rowIndex):
-			A =list(map(lambda x,y:x+y ,[0]+res[-1],res[-1]+[0]))  #convert map to list
-			res +=[A]
-		return res[rowIndex]
+	res =[[1]]
+	for i in range(rowIndex):
+		A =list(map(lambda x,y:x+y ,[0]+res[-1],res[-1]+[0]))  #convert map to list
+		res +=[A]
+	return res[rowIndex]
